ea283b0d6a33ad9a5c47b2f9805b63c1cf338b66
desse <lea.desse@esme.fr>
2018-11-12 09:21:02 +0100

    commit du projet Android

---
+++ TP_Android/app/src/test/java/com/desse/lea/tp_communication_ingredients/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.desse.lea.tp_communication_ingredients
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
